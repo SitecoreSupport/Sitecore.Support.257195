4d159c0167faea265b274c4a488536202f7d172e
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-10-03 13:03:37 +0300

    use target item ID even if it doesn't exist

---
+++ src/Sitecore.Support.257195/Data/Fields/ReferenceField.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.Support.Data.Fields
+{
+  using Sitecore.Data;
+  using Sitecore.Data.Fields;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Links;
+
+  /// <summary>
+  /// The reference field.
+  /// </summary>
+  public class ReferenceField : Sitecore.Data.Fields.ReferenceField
+  {
+    public ReferenceField(Field innerField) : base(innerField)
+    {
+    }
+
+    public override void RemoveLink([NotNull] ItemLink itemLink)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+
+      ID tempTargetID = (TargetID == ID.Null && ID.IsID(Path)) ? new ID(Path) : TargetID;
+
+      if (itemLink.TargetItemID == tempTargetID)
+      {
+        this.Clear();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.257195/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.257195")]
+[assembly: AssemblyProduct("Sitecore.Support.257195")]
+[assembly: ComVisible(false)]
